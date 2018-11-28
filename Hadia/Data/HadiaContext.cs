152f1319baf750bc43ea71972438757a16f1468b
Selik <selik.kilayil@gmail.com>
2018-11-28 16:54:55 +0530

    Add project files.

---
+++ Hadia/Data/HadiaContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hadia.Data
+{
+    public class HadiaContext : DbContext
+    {
+        public HadiaContext(DbContextOptions<HadiaContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+}
---
+++ Hadia/Models/DomainModels/Prof_Member.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hadia.Models.DomainModels
+{
+    public class ProfMember
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AdNo { get; set; }
+        public string PresentAddress { get; set; }
+        public string PermanentAddress { get; set; }
+        public MaritalStatus Type { get; set; }
+    }
+
+    public enum MaritalStatus :byte
+    {
+        [DisplayName("Single")]
+        Single=1,
+        [DisplayName("Married")]
+        Married = 2,
+        [DisplayName("Divorced")]
+        Divorced = 3
+    }
+}
