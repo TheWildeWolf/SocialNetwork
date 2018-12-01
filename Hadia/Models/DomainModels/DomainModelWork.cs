16f1fdc49473ee946480dd2fa7dda894e5558ebf
Selik <selik.kilayil@gmail.com>
2018-12-01 14:52:43 +0530

    added Post like file

--- Hadia/Models/DomainModels/DomainModelWork.cs
+++ Hadia/Models/DomainModels/DomainModelWork.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,19 +7,6 @@ namespace Hadia.Models.DomainModels
     public class DomainModelWork
     {
         
-    }
-
-    public class Post_Like
-    {
-        public int Id { get; set; }
-        public int PostId { get; set; }
-        public int MemberId { get; set; }
-
-        public DateTime CDate { get; set; }
-        public bool Like { get; set; }
-
-        public Mem_Master Member { get; set; }
-        public Post_Master Post { get; set; }
     }
  
 }
