38b23586c8cee7fb8652e6512544234bb8addbe0
Selik <selik.kilayil@gmail.com>
2018-12-03 12:29:37 +0530

    Last Tables

---
+++ Hadia/Models/DomainModels/Sett_DeviceInfoLogs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hadia.Models.DomainModels
+{
+    public class Sett_DeviceInfoLog
+    {
+        public int Id { get; set; }
+        public int MemberId { get; set; }
+        public string DeviceKey { get; set; }
+
+        public DateTime CDate { get; set; }
+
+        public Mem_Master Member { get; set; }
+    }
+}
+
