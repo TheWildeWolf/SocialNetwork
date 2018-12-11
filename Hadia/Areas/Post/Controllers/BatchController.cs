7b44f9daed15bc3bc9e3fe0c5d0d7a5f7e57f9cf
fousiya <fousiya@DOTNET-4>
2018-12-11 18:15:34 +0530

    cc

---
+++ Hadia/Areas/Post/Controllers/BatchController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hadia.Areas.Post.Controllers
+{
+    public class BatchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+
+        }
+    }
+}
---
+++ Hadia/Models/ViewModels/BatchViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hadia.Models.ViewModels
+{
+    public class BatchViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Group Name")]
+        [Required(ErrorMessage = "Group Name Required")]
+        public string GroupName { get; set; }
+        public GroupType Type { get; set; }
+        public GroupPrivacy OpenOrClosed { get; set; }
+        public string Description { get; set; }
+        public DateTime? FormedOn { get; set; }
+        public string PassoutYear { get; set; }
+    }
+    public enum GroupType : byte
+    {
+        Chapter = 1,
+        Group = 2,
+        Batch = 3
+
+    }
+
+    public enum GroupPrivacy : byte
+    {
+        Open = 1,
+        Closed = 2
+    }
+
+}
