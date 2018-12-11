27e3b3745f0bb2761f44099984dcc4cc3f13757b
fousiya <fousiya@DOTNET-4>
2018-12-11 12:37:43 +0530

    state and country

---
+++ Hadia/Models/ViewModels/CountryCodeViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hadia.Models.ViewModels
+{
+    public class CountryCodeViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Country Name")]
+        [Required(ErrorMessage = "Country Name Required")]
+        public string CountryName { get; set; }
+        [Display(Name = "Country Code")]
+        [Required(ErrorMessage = "Country Code Required")]
+        public string CountryCode { get; set; }
+        [Display(Name = "Time Zone")]
+        [Required(ErrorMessage = "Time Zone Required")]
+        public string TimeZone { get; set; }
+    }
+}
