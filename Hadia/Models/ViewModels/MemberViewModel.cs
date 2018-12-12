25d64d0c519ad5b70e6e1bcbc71d68915c9373fb
fousiya <fousiya@DOTNET-4>
2018-12-12 16:42:50 +0530

    all from last

---
+++ Hadia/Models/ViewModels/MemberViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hadia.Models.ViewModels
+{
+    public class MemberViewModel
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? UgCollageId { get; set; }
+        public string Phone { get; set; }
+        public int? GroupId { get; set; } 
+        public string Email { get; set; }
+        public bool IsVarified { get; set; }
+        public string UgCollegeName { get; set; }
+        public string GroupName { get; set; }
+    }
+}
